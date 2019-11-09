8646bbbb35e3fef711c38c027241b464552ca8b3
Kishor Naik <kishor.naik011.net@outlook.com>
2019-11-09 19:48:20 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/ControllerDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo.Controllers
+{
+
+
+    [Controller] // Define Controller Attribute, Now it become Controller action without Controller class inheritance.
+    public class ControllerDemo
+    {
+        // http://localhost:26213/ControllerDemo/Index
+        public string Index()
+        {
+            return "Hello";
+        }
+
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Controllers/TrafficController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo.Controllers
+{
+    [NonController] // Define Non Controller attribute, it will not become as Controller.
+    public class TrafficController
+    {
+
+        // Run the following action in browser, it will execute becuase of "Controller" suffix in a class.
+        // http://localhost:26213/Traffic/Index
+
+        public String Index()
+        {
+            return "Hello";
+        }
+    }
+}
